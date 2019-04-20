d6aff7299a98b73b03ca326e4284879d0d1c1492
orlinskas <orlinskaslove@gmail.com>
2019-04-20 18:56:07 +0300

    add progress bar

--- app/src/main/java/com/example/weather_statistics/MainActivity.java
+++ app/src/main/java/com/example/weather_statistics/MainActivity.java
@@ -6,15 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewApi1, textViewApi2, textViewApi3, textViewApi4, textViewApi5;
     private Button buttonGetApi;
+    private ProgressBar progressBarHorizontal;
+
+    WeatherRequestSenderAccuWeather requestSenderAccuWeather = new WeatherRequestSenderAccuWeather();
+    WeatherRequestSenderOpenWeather requestSenderOpenWeather = new WeatherRequestSenderOpenWeather();
+
+    WeatherParserAcuuWeather parserAcuuWeather = new WeatherParserAcuuWeather();
+    WeatherParserOpenWeather parserOpenWeather = new WeatherParserOpenWeather();
 
 
-    class ApiGetResponse extends AsyncTask <Void, Void, Void>{
+    class ApiGetResponse extends AsyncTask <Void, Integer, Void>{
 
         @Override
         protected void onPreExecute() {
@@ -24,21 +34,34 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Void doInBackground(Void... voids) {
-            WeatherRequestSenderAccuWeather requestSenderAccuWeather = new WeatherRequestSenderAccuWeather();
-            WeatherRequestSenderOpenWeather requestSenderOpenWeather = new WeatherRequestSenderOpenWeather();
 
-            WeatherParserAcuuWeather parserAcuuWeather = new WeatherParserAcuuWeather();
-            WeatherParserOpenWeather parserOpenWeather = new WeatherParserOpenWeather();
+            try {
+                int value = 0;
 
-            parserAcuuWeather.parse(requestSenderAccuWeather.requestWeather(Constants.ACCUWEATHER_KHARKIV_ID));
-            parserOpenWeather.parse(requestSenderOpenWeather.requestWeather(Constants.OPENWEATHERMAP_KHARKIV_ID));
+                parserAcuuWeather.parse(requestSenderAccuWeather.requestWeather(Constants.ACCUWEATHER_KHARKIV_ID));
+                publishProgress(++value);
+
+                parserOpenWeather.parse(requestSenderOpenWeather.requestWeather(Constants.OPENWEATHERMAP_KHARKIV_ID));
+                publishProgress(++value);
+
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBarHorizontal.setProgress(values[0]);
         }
 
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
             textViewApi2.setText("work done");
+            progressBarHorizontal.setProgress(0);
         }
 
     }
@@ -53,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
         textViewApi3 = findViewById(R.id.activity_main_et_api3);
         textViewApi4 = findViewById(R.id.activity_main_et_api4);
         textViewApi5 = findViewById(R.id.activity_main_et_api5);
+        progressBarHorizontal = findViewById(R.id.progressBar2);
+        progressBarHorizontal.setMax(2);
     }
 
     public void onClickButtonGetApi(View view) {
