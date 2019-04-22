0bff0b9963de110292eb9821ee0b45b5e83b1880
orlinskas <orlinskaslove@gmail.com>
2019-04-22 03:57:45 +0300

    have data base still testing

--- app/src/main/java/com/example/weather_statistics/MainActivity.java
+++ app/src/main/java/com/example/weather_statistics/MainActivity.java
@@ -10,14 +10,17 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.weather_statistics.date.DatabaseAdapter;
 import com.example.weather_statistics.date.DatabaseHelper;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textViewApi1, textViewApi2, textViewApi3, textViewApi4, textViewApi5;
+    private TextView textViewApi1, textViewApi2, textViewApi3, textViewApi4, textViewApi5
+            , tvData1, tvData2, tvData3, tvData4, tvData5, tvData6;
     private Button buttonGetApi, buttonData;
     private ProgressBar progressBarHorizontal;
     private Date effectiveDate = new Date();
@@ -26,11 +29,17 @@ public class MainActivity extends AppCompatActivity {
     private String sourse = "OpenWeatherMap";
     private String location = null;
 
+    ArrayList<Weather> weathers = new ArrayList<>();
+
     WeatherRequestSenderAccuWeather requestSenderAccuWeather = new WeatherRequestSenderAccuWeather();
     WeatherRequestSenderOpenWeather requestSenderOpenWeather = new WeatherRequestSenderOpenWeather();
 
     WeatherParserAcuuWeather parserAcuuWeather = new WeatherParserAcuuWeather();
     WeatherParserOpenWeather parserOpenWeather = new WeatherParserOpenWeather();
+
+    DatabaseAdapter database;
+
+
 
 
     class ApiGetResponse extends AsyncTask <Void, Integer, Void>{
@@ -38,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            textViewApi1.setText("work");
+            buttonGetApi.setVisibility(View.INVISIBLE);
         }
 
         @Override
@@ -52,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 //parserAcuuWeather.parse(requestSenderAccuWeather.requestWeather(Constants.ACCUWEATHER_KHARKIV_ID));
                 publishProgress(++value);
 
-                parserOpenWeather.parse(requestSenderOpenWeather.requestWeather(Constants.OPENWEATHERMAP_KHARKIV_ID));
+                weathers = parserOpenWeather.parse(requestSenderOpenWeather.requestWeather(Constants.OPENWEATHERMAP_KHARKIV_ID));
                 publishProgress(++value);
 
                 TimeUnit.SECONDS.sleep(1);
@@ -71,8 +80,15 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
-            textViewApi2.setText("work done");
+            buttonGetApi.setVisibility(View.VISIBLE);
             progressBarHorizontal.setProgress(0);
+
+            textViewApi1.setText(weathers.get(1).getEffectiveDate());
+            textViewApi2.setText(weathers.get(1).getDate());
+            textViewApi3.setText(Float.toString(weathers.get(1).getTemperature()));
+            textViewApi4.setText(weathers.get(1).getLocation());
+            textViewApi5.setText(weathers.get(1).getSource());
+
         }
 
     }
@@ -89,18 +105,37 @@ public class MainActivity extends AppCompatActivity {
         textViewApi5 = findViewById(R.id.activity_main_et_api5);
         buttonGetApi = findViewById(R.id.activity_main_btn);
         buttonData = findViewById(R.id.btn_saveData);
+        tvData1 = findViewById(R.id.activity_main_et_data1);
+        tvData2 = findViewById(R.id.activity_main_et_data2);
+        tvData3 = findViewById(R.id.activity_main_et_data3);
+        tvData4 = findViewById(R.id.activity_main_et_data4);
+        tvData5 = findViewById(R.id.activity_main_et_data5);
+        tvData6 = findViewById(R.id.activity_main_et_data6);
         progressBarHorizontal = findViewById(R.id.progressBar2);
         progressBarHorizontal.setMax(2);
+        database = new DatabaseAdapter(this);
     }
 
     public void onClickButtonGetApi(View view) {
          new ApiGetResponse().execute();
-
     }
 
     public void onClickButtonSaveData(View view) {
-
+        database.open();
+        database.insert(weathers.get(1));
+        database.close();
     }
 
+    public void onClickButtonGetData(View view) {
+        database.open();
+        Weather weather = database.getWeather(1);
+        //tvData1.setText(weather.getId());
+        tvData2.setText(weather.getEffectiveDate());
+        tvData3.setText(weather.getDate());
+        tvData4.setText(Float.toString(weather.getTemperature()));
+        tvData5.setText(weather.getLocation());
+        tvData5.setText(weather.getSource());
+        database.close();
+    }
 
 }
